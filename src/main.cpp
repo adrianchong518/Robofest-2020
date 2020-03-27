d38f4db3ca98520088a1aa6e91b41e897beaa0a6
Adrian Chong <adrianchong518@gmail.com>
2020-03-27 21:34:55 +0800

    Refactored constants into separate header file

--- src/main.cpp
+++ src/main.cpp
@@ -9,79 +9,8 @@
 #include <GY53.h>
 #include <JY901.h>
 
+#include "Constants.h"
 #include "Motor.h"
-
-#define PIN_RAIL_PUL 45
-#define PIN_RAIL_DIR 46
-#define PIN_RAIL_HOME_L 47
-#define PIN_RAIL_HOME_R A0
-
-#define PIN_TURN_CW 48
-#define PIN_TURN_CCW 49
-#define PIN_TURN_HOME A11
-#define TURN_HOME_THRESHOLD 700
-
-#define PIN_IR_FL A12
-#define PIN_IR_FR A10
-#define PIN_IR_BL A14
-#define PIN_IR_BR A15
-
-#define PIN_GUIDE_LEFT 27
-#define PIN_GUIDE_RIGHT 12
-
-#define PIN_HOLDER_LEFT 52
-#define PIN_HOLDER_RIGHT 53
-
-#define PIN_MEASURE_SERVO 26
-
-#define PIN_HITTER_MOTOR_INA 50
-#define PIN_HITTER_MOTOR_INB 51
-#define PIN_HITTER_MOTOR_PWM 4
-
-#define HITTER_PID_KP 1
-#define HITTER_PID_KI 0
-#define HITTER_PID_KD 0
-#define HITTER_PID_MIN -255
-#define HITTER_PID_MAX 255
-
-#define PIN_WHEEL_FL_INA 24
-#define PIN_WHEEL_FL_INB 25
-#define PIN_WHEEL_FL_PWM 2
-
-#define PIN_WHEEL_FR_INA 11
-#define PIN_WHEEL_FR_INB 8
-#define PIN_WHEEL_FR_PWM 9
-
-#define PIN_WHEEL_BL_INA 22
-#define PIN_WHEEL_BL_INB 23
-#define PIN_WHEEL_BL_PWM 3
-
-#define PIN_WHEEL_BR_INA 7
-#define PIN_WHEEL_BR_INB 5
-#define PIN_WHEEL_BR_PWM 6
-
-#define PORT_ENCODER_BUS PORTC
-#define DDR_ENCODER_BUS DDRC
-#define PIN_ENCODER_BUS PINC
-#define PIN_ENCODER_CLK 10
-#define PIN_ENCODER_SEL 38
-
-#define PIN_HITTER_ENCODER_OE 39
-#define PIN_HITTER_ENCODER_RST 40
-
-#define PIN_MEASURE_ENCODER_OE 41
-#define PIN_MEASURE_ENCODER_RST 42
-
-#define PIN_LASER_PHOTORESISTOR A13
-
-#define SERIAL_GYROSCOPE Serial3
-
-#define SERIAL_IR_DISTANCE Serial2
-#define SERIAL_IR_DISTANCE_BAUDRATE 115200
-
-#define PIN_BUZZER 13
-
-#define PIN_START_BUTTON A1
 
 StepperRail rail(PIN_RAIL_PUL, PIN_RAIL_DIR, PIN_RAIL_HOME_R, 1);
 StepperDegree turnTable(PIN_TURN_CW, PIN_TURN_CCW, PIN_TURN_HOME,
