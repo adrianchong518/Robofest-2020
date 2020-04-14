2978d8cc941beff538453eb246f400b9d54be2be
Adrian Chong <adrianchong518@gmail.com>
2020-04-14 21:40:14 +0800

    Changed pin definitions

--- include/Constants.h
+++ include/Constants.h
@@ -3,8 +3,8 @@
 
 // Pin Definitions
 
-#define PIN_RAIL_PUL 45
-#define PIN_RAIL_DIR 46
+#define PIN_RAIL_PUL 43
+#define PIN_RAIL_DIR 44
 #define PIN_RAIL_HOME_L 47
 #define PIN_RAIL_HOME_R A0
 
@@ -25,8 +25,8 @@
 
 #define PIN_MEASURE_SERVO 26
 
-#define PIN_HITTER_MOTOR_INA 50
-#define PIN_HITTER_MOTOR_INB 51
+#define PIN_HITTER_MOTOR_INA 45
+#define PIN_HITTER_MOTOR_INB 46
 #define PIN_HITTER_MOTOR_PWM 4
 
 #define PIN_WHEEL_FL_INA 24
@@ -73,7 +73,7 @@
 #define DDR_SW_BTN DDRF
 #define PIN_SW_BTN PINF
 
-#define BITS_DIP_SW 4  // 4 - 7
+#define BITS_DIP_SW 4 // 4 - 7
 #define PIN_START_BUTTON A0
 #define PIN_BUTTON_1 A1
 #define PIN_BUTTON_2 A2
@@ -89,22 +89,22 @@
 #define TURN_LOWER_LIMIT_DEG -30
 #define TURN_UPPER_LIMIT_DEG 30
 
-#define HITTER_PID_KP 1  // TODO Need Testing
-#define HITTER_PID_KI 0  // TODO Need Testing
-#define HITTER_PID_KD 0  // TODO Need Testing
+#define HITTER_PID_KP 1 // TODO Need Testing
+#define HITTER_PID_KI 0 // TODO Need Testing
+#define HITTER_PID_KD 0 // TODO Need Testing
 #define HITTER_PID_MIN -255
 #define HITTER_PID_MAX 255
 
-#define HITTER_TARGET_DEG_MIN 0  // TODO Need Data
-#define HITTER_TARGET_DEG_MAX 0  // TODO Need Data
+#define HITTER_TARGET_DEG_MIN 0 // TODO Need Data
+#define HITTER_TARGET_DEG_MAX 0 // TODO Need Data
 #define HITTER_DEG_ALLOWED_ERROR 1
 
-#define MECANUM_ROT_PID_KP 1  // TODO Need Testing
-#define MECANUM_ROT_PID_KI 0  // TODO Need Testing
-#define MECANUM_ROT_PID_KD 0  // TODO Need Testing
+#define MECANUM_ROT_PID_KP 1 // TODO Need Testing
+#define MECANUM_ROT_PID_KI 0 // TODO Need Testing
+#define MECANUM_ROT_PID_KD 0 // TODO Need Testing
 
-#define MECANUM_ROT_DIFF_MIN -510  // -255*2
-#define MECANUM_ROT_DIFF_MAX 510   // 255*2
+#define MECANUM_ROT_DIFF_MIN -510 // -255*2
+#define MECANUM_ROT_DIFF_MAX 510  // 255*2
 
 #define GUIDE_LEFT_RETRACTED_POS 80
 #define GUIDE_LEFT_EXTENDED_POS 50
